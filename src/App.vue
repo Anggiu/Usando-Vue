2b8af597becdc44922d9789a8eb2a05701c349e8
Anggiu <angvra23@gmail.com2>
2022-10-09 19:07:55 -0500

    clase 1 Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <div>
+  <h1>{{mensaje}}</h1>
+  <h2 v-text="texto"></h2>
+  <input type="text" v-model="inputText"> 
+  <img :src="img" alt="">
+ </div>
 </template>
+<script>
+
+export default ({
+ name: "App", //a cada elemento se le debe colocar el nombre
+
+ data() {//función donde retorna variables
+  return{
+    mensaje: "Hola Vue",
+    texto: "hola texto",
+    inputText: "",//para capturar el valor de un input
+    img: "https://picsum.photos/id/237/200/300" //vbind aributos dinamicos
+  }
+ }
+
+})
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
